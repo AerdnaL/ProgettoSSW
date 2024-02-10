0eef1f63f0d10349882cc6807293b08ad9c63fae
Andrea Lelli <andrea.ventisette.lelli@gmail.com>
2024-02-10 23:39:52 +0100

    Inizio implementazione metodo ricerca
    ancora non appare p

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AjaxResponse } from 'rxjs/ajax';
+import { BibliotecaService } from '../biblioteca.service';
 
 @Component({
   selector: 'app-ricerca',
@@ -11,9 +13,26 @@ import { CommonModule } from '@angular/common';
 export class RicercaComponent implements OnInit {
   @Input() vediRicerca: boolean = false;
 
-  constructor() { }
+  occorrenze: string = '';
 
-  ngOnInit() {
+  cercaLibro(event: KeyboardEvent) {
+    this.bs.getData().subscribe({
+      next: (ajaxRes: AjaxResponse<any>) => {
+        const risposta = ajaxRes.response;
+        const data = JSON.parse(risposta);
+        const cercato = data.filter(
+          (libro: any) =>
+            libro.autore.toLowerCase().includes((event.target as HTMLInputElement).value) ||
+            libro.titolo.toLowerCase().includes((event.target as HTMLInputElement).value)
+        );
+        console.log(cercato);
+        let prova = (event.target as HTMLInputElement).value;
+        console.log(prova);
+      },
+    });
   }
 
+  constructor(private bs: BibliotecaService) {}
+
+  ngOnInit() {}
 }

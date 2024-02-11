559242ee9823fa56f23bf7c8258571dc4b715739
Andrea Lelli <andrea.ventisette.lelli@gmail.com>
2024-02-11 11:53:23 +0100

    Cambiamenti a array filtrato e component che appare

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -1,31 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AjaxResponse } from 'rxjs/ajax';
+import { TrovatoLiberoComponent } from './trovato-libero/trovato-libero.component'
 import { BibliotecaService } from '../biblioteca.service';
+import { AutoriLibri } from '../autori-libri';
 
 @Component({
   selector: 'app-ricerca',
   templateUrl: './ricerca.component.html',
   styleUrls: ['./ricerca.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TrovatoLiberoComponent],
 })
 export class RicercaComponent implements OnInit {
   @Input() vediRicerca: boolean = false;
 
-  occorrenze: string = '';
+  occorrenze: any = '';
+  cercato: AutoriLibri = new AutoriLibri('', '', '', '');
 
   cercaLibro(event: KeyboardEvent) {
     this.bs.getData().subscribe({
       next: (ajaxRes: AjaxResponse<any>) => {
         const risposta = ajaxRes.response;
         const data = JSON.parse(risposta);
-        const cercato = data.filter(
+        this.cercato = data.filter(
           (libro: any) =>
             libro.autore.toLowerCase().includes((event.target as HTMLInputElement).value) ||
             libro.titolo.toLowerCase().includes((event.target as HTMLInputElement).value)
         );
-        this.occorrenze = cercato.length;
+        this.occorrenze = Object.keys(this.cercato).length;
       },
     });
   }
--- src/root/ricerca/trovato-libero/trovato-libero.component.ts
+++ src/root/ricerca/trovato-libero/trovato-libero.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutoriLibri } from '../../autori-libri';
 
 @Component({
   selector: 'app-trovato-libero',
   templateUrl: './trovato-libero.component.html',
-  styleUrls: ['./trovato-libero.component.css']
+  styleUrls: ['./trovato-libero.component.css'],
+  imports: [CommonModule],
+  standalone: true
 })
 export class TrovatoLiberoComponent implements OnInit {
+  @Input() occorrenze: any;
+  @Input() cercato: AutoriLibri | undefined;
 
   constructor() { }
 
